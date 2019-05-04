094f83ae0045b3f5e9d5d14b89ffc08156534638
sakshisb <bhargavasakshi2110@gmail.com>
2019-05-04 11:28:07 +0530

    initial commit

---
+++ product_list.js
@@ -0,0 +1,18 @@
+var products = {
+	offer : {
+		price: "24449",
+		name: "Micromax 81cm (32) LED TV",
+	},
+	
+                        home : {
+		price: "2794",
+		name: "Fortune Pack of 40 Dinner Set",
+	}
+,
+                        TV: {
+		        price: "24449",
+		name: "Micromax 81cm (32) LED TV",
+	},
+                        TV: {                   price: "19999",         name: "LG 80cm (32 inch) HD Ready LED Smart TV", },
+                        elec: {                 price: "325",           name: "SmartBuy 1000 W Dry Iron",       },
+                        Books: {                price: "850",           name: "Quantitative Aptitude for Competitive Examinations 2017 Edition",  }}
